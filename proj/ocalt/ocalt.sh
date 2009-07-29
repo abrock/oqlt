88858a381aadc3a8290ea027b11b2b16d95b2602
Tim Weber <scy@scytale.name>
2009-07-29 13:28:57 +0200

    ocalt: Verstehe auch Uhrzeiten xx:xx-yy:yy.

--- proj/ocalt/ocalt.sh
+++ proj/ocalt/ocalt.sh
@@ -28,7 +28,8 @@ for file in $(find events -name '_.event' | sort); do
 				text="$(echo "$text" | sed -r -e 's/ bis / *1 UNTIL /')"
 			elif echo "$text" | egrep -q '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'; then
 				:
-			elif echo "$text" | egrep -q '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{1,2}:[0-9]{2} bis [0-9]{1,2}:[0-9]{2}$'; then
+			elif echo "$text" | egrep -q '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{1,2}:[0-9]{2}( bis | *- *)[0-9]{1,2}:[0-9]{2}$'; then
+				text="$(echo "$text" | sed -r -e 's/(:[0-9]{2}) *- */\1 bis /')"
 				day="$(echo "$text" | cut -d ' ' -f 1)"
 				time1="$(echo "$text" | cut -d ' ' -f 2)"
 				time2="$(echo "$text" | cut -d ' ' -f 4 | sed -e 's/^24:00$/23:59/')"
