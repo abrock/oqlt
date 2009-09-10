9b747bc4091c3fe824d1173f1eb7bd64fdd393f8
Tim Weber <scy@scytale.name>
2009-09-10 11:31:41 +0200

    Statistiken nach Spielen und Spielern.

--- events/20090908-bowling/plot.sh
+++ events/20090908-bowling/plot.sh
@@ -1,14 +1,48 @@
 #!/bin/sh
 
-plotcmd="plot 'score.txt' using 1 linewidth 3, \\
-$(for n in $(seq 2 $(head -n 1 score.txt | wc -w)); do echo "'' using $n linewidth 3, \\"; done | head -c -4)"
+plotcmd() {
+	lw=''
+	[ "$1" = 'lw' ] && lw=" linewidth 3"
+	echo "plot 'score.txt' using 1$lw, \\
+	$(for n in $(seq 2 $(head -n 1 score.txt | wc -w)); do echo "'' using $n$lw, \\"; done | head -c -4)"
+}
 
-gnuplot <<EOF
-set terminal svg
-set output 'score.svg'
-set key top left
-set key autotitle columnheader
-set style data lines
-set xtics 1 format ''
-$plotcmd
+plot() {
+	key='top left'
+	datastyle='histogram'
+	histstyle='rowstacked'
+	plotparam=''
+	rotate=0
+	case "$1" in
+	score)
+		datastyle='lines'
+		plotparam='lw'
+		;;
+	games)
+		key="outside $key"
+		;;
+	sum)
+		histstyle='columnstacked'
+		rotate=90
+		;;
+	*)
+		return 1
+		;;
+	esac
+	gnuplot <<EOF
+	set terminal svg
+	set output "$1.svg"
+	set key $key autotitle columnheader
+	set grid
+	set style data $datastyle
+	set style histogram $histstyle
+	set style fill solid border -1
+	set boxwidth 0.8
+	set xtics 1 format '' rotate by $rotate
+	$(plotcmd $plotparam)
 EOF
+}
+
+plot score
+plot games
+plot sum
